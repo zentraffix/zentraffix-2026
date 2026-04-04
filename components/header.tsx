bab6b309d7768d819609c2157ca16e59c794f4a8
zentraffix <zentraffix@gmail.com>
2026-04-04 12:12:49 +0530

    Updated header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button';
 const navigation = [
   { name: 'Home', href: '#home' },
   { name: 'Services', href: '#services' },
+  { name: 'Portfolio', href: '#portfolio' },
   { name: 'About', href: '#about' },
   { name: 'Contact', href: '#contact' },
 ];
