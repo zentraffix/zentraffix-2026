189d34ebe06e79c041f600224ec6f61963f83089
zentraffix <zentraffix@gmail.com>
2026-04-04 12:07:58 +0530

    Updated portfolio-section.tsx

--- components/portfolio-section.tsx
+++ components/portfolio-section.tsx
@@ -66,7 +66,7 @@ export default function PortfolioSection() {
               className={`px-6 py-2 rounded-full text-sm font-medium transition-all duration-300 border
   ${
     active === cat
-      ? 'bg-gradient-to-r from-indigo-500 to-purple-500 text-white border-transparent shadow-lg scale-105'
+      ? 'bg-gradient-to-r from-indigo-500 to-purple-500 text-black border-transparent shadow-lg scale-105'
       : 'border-gray-500 text-white hover:border-white hover:bg-white/10 hover:scale-105'
   }
 `}
