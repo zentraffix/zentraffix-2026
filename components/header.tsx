0f9664f35968e9c6714479d11e8453e3b043e23e
zentraffix <zentraffix@gmail.com>
2026-04-04 12:13:46 +0530

    Updated header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -10,6 +10,7 @@ const navigation = [
   { name: 'Home', href: '#home' },
   { name: 'Services', href: '#services' },
   { name: 'Portfolio', href: '#portfolio' },
+
   { name: 'About', href: '#about' },
   { name: 'Contact', href: '#contact' },
 ];
